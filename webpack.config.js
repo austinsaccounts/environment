467debe20193f5d010d14ec7d156eda73bb8a2ff
austin cummings <Austinsaccts@gmail.com>
2018-09-22 14:53:40 -0700

    pray

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin
+  ]
+}
+
+module.exports = config;
